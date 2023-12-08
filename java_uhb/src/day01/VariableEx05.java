b33a31b3d6f080fed0cc198cdcaea3772a7a0c5f
umheabin <luv9685@naver.com>
2023-12-08 16:28:59 +0900

    Create VariableEx05.java

---
+++ java_uhb/src/day01/VariableEx05.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class VariableEx05 {
+	public static void main(String[] args) {
+		String str1= null;
+		System.out.println(str1);
+		//기본형 변수에는 null로 저장할 수 없다.
+		//int num1 = null;
+		String str2 = "Hello";
+		System.out.println(str2);
+		//String 클래스에 문자를 저장할 수 없다.
+		//String str3 = 'A';
+		String str4 = "A";
+		System.out.println(str4);
+		//String 클래스에 빈 문자열을 저장할 수 있다.
+		String str5 = "";
+		System.out.println(str5);
+		//문자열에 정수 또는 실수 또는 문자 또는 논리값을 더하면 문자열이 됨.
+		//빈 문자열 + 'A'는 "A"가 되어서 저장이 가능해진다.
+		String str6 = "" + 'A';
+		System.out.println(str6);
+	}
+
+}
