05ac4367f21368f4d5e6f9f5dc7e4e5652096744
umheabin <luv9685@naver.com>
2023-12-08 14:41:30 +0900

    java 공부중

--- java_uhb/src/day01/HelloWorld.java
+++ java_uhb/src/day01/HelloWorld.java
@@ -1,8 +1,13 @@
 package day01;
 
-public class HelloWorld {
-    public static void main(String[] args) {
-    	System.out.println("hello_world~~~!!!!");
-    }
+//한 줄 주석//
+/*여러줄 주석*/
+public class HelloWorld { /**메소드를 설명할 때 사용하는 주석**/
+	public static void main(String[] args) {
+		System.out.println("hello_world~~~!!!!");
+		//sysout은 콘솔에 출력할 때 사용//
+		
+		
+	}
 
 }
---
+++ java_uhb/src/day01/VariableEx01.java
@@ -0,0 +1,22 @@
+package day01;
+
+public class VariableEx01 {
+
+	public static void main(String[] args) {
+		/*변수 선언 방법*
+		1.변수 선언만 함.
+		*변수타입 변수명;*
+		2.변수를 선언하고 초기화함
+		*변수타입 변수명 = 값*/
+		//문자형 변수 ch1를 선언//
+		char ch1;
+		//문자형 변수 ch2를 선언하고 문자 A로 초기화
+		char ch2 = 'A';
+		//변수는 초기화를 하고 사용해야 합니다//
+		System.out.println(ch2);
+		ch1 = '+';
+		System.out.println(ch1);
+
+	}
+
+}
