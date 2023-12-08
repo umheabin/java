be13e2176696a90e9813c1d6f68dbc00d9b4ffcd
umheabin <luv9685@naver.com>
2023-12-08 14:51:37 +0900

    java 공부중

--- java_uhb/src/day01/VariableEx01.java
+++ java_uhb/src/day01/VariableEx01.java
@@ -16,6 +16,20 @@ public class VariableEx01 {
 		System.out.println(ch2);
 		ch1 = '+';
 		System.out.println(ch1);
+		//""문자열을 의미,""안에 한글자가 들어간다해서 문자가 되는것이 
+		//아니라 한글자인 문자열이다.//
+		//char ch3 = "A";
+		//''안에는 한 글자가 들어가야 한다.
+		//char ch4 = '12';
+		//유니코드 65에 해당하는 문자를 저장
+		char ch5 = '\u0065';
+		System.out.println(ch5);
+		char ch6 = 61;
+		System.out.println(ch6);
+		//유니코드 65에 해당하는 문자를 저장
+		//\가 들어간 문자들이 있음. \n 엔터 , \t 탭키만큼의 공백, \\ :\문자 등
+		char ch7 = '\n';
+		System.out.println(ch7);
 
 	}
 
