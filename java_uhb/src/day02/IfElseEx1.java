f0c052d20826fd26d06ae8957649f61c43907421
umheabin <luv9685@naver.com>
2023-12-11 17:36:35 +0900

    자바 2일차 if문 예제

---
+++ java_uhb/src/day02/IfElseEx1.java
@@ -0,0 +1,14 @@
+package day02;
+
+public class IfElseEx1 {
+	public static void main(String[] args) {
+		int num = 0;
+		
+		if(num == 0) {
+			System.out.println("0입니다.");
+		} else {
+			System.out.println(num + "는 0이 아닙니다.");
+		}
+
+	}
+}
