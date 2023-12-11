145091cd29996c51bc14a235f57c9d685d4a5bdb
umheabin <luv9685@naver.com>
2023-12-11 12:45:47 +0900

    자바 2일차 비교 연산자 예제

---
+++ java_uhb/src/day02/RalationalOperatorEx.java
@@ -0,0 +1,17 @@
+package day02;
+
+public class RalationalOperatorEx {
+	public static void main(String[] args) {
+		int num1 = 1 , num2 = 2;
+		
+		System.out.println(num1 + " > " + num2  + " : " + (num1 > num2));
+		System.out.println(num1 + " >= " + num2  + " : " + (num1 >= num2));
+		System.out.println(num1 + " < " + num2  + " : " + (num1 < num2));
+		System.out.println(num1 + " <= " + num2  + " : " + (num1 <= num2));
+		System.out.println(num1 + " == " + num2  + " : " + (num1 == num2));
+		System.out.println(num1 + " ! " + num2  + " : " + (num1 != num2));
+		
+	}
+	
+
+}
