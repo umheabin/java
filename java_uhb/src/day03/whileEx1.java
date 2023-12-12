2168049596f1255d1f5bd7e2042640af0abcbf18
umheabin <luv9685@naver.com>
2023-12-12 14:03:52 +0900

    자바 3일차 while문 예제

---
+++ java_uhb/src/day03/whileEx1.java
@@ -0,0 +1,15 @@
+package day03;
+
+public class whileEx1 {
+	public static void main(String[] args) {
+		int i = 1;
+		
+		while(i<5) {
+			System.out.println("Hello world");
+			++i;
+			
+		}
+		
+	}
+
+}
