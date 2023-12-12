ead7ef5ea8693fc3cdf0b91161e72d210ee95597
umheabin <luv9685@naver.com>
2023-12-12 17:09:20 +0900

    자바 3일차 ForEx 예제

---
+++ java_uhb/src/day03/ForEx1.java
@@ -0,0 +1,10 @@
+package day03;
+
+public class ForEx1 {
+	/* Hello world 5번 출력 예제*/
+	public static void main(String[] args) {
+	int i;
+	for(i =1;  i<=5; i++) {
+		System.out.println("hello world");
+	}
+}}
