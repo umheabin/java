d42ff446e1e6426979918da19dbd2fe1a892869c
umheabin <luv9685@naver.com>
2023-12-12 14:04:26 +0900

    자바 3일차 while문 2의 곱셈

---
+++ java_uhb/src/day03/WhileMultipleTableEx1.java
@@ -0,0 +1,14 @@
+package day03;
+
+public class WhileMultipleTableEx1 {
+	public static void main(String[] args) {
+		int num = 2;
+   	    int i = 1;
+   	    while(i <= 9) {
+   		 System.out.println(num + "*" + i + "=" + (num*i));
+   		 i++;
+ 
+	}
+
+}
+}
