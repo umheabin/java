e76c9b330c857c4c8fcd8dcb124ff1d31ad8fddf
umheabin <luv9685@naver.com>
2023-12-12 13:52:27 +0900

    자바 3일차 홀짝 출력

---
+++ java_uhb/src/day03/SwitchEx2.java
@@ -0,0 +1,31 @@
+package day03;
+
+import java.util.Scanner;
+
+public class SwitchEx2 {
+	public static void main(String[] args) {
+		//정수를 입력받아 입력받은 정수의 홀짝 판별 예제를 switch문을 이용하여 작성하시오.
+	Scanner scan = new Scanner(System.in);
+	System.out.println("정수 입력:");
+	int num = scan.nextInt();
+	
+	if(num % 2 == 0) {
+		System.out.println(num + "는 짝수");
+	}else {
+		System.out.println(num + "는 홀수");
+	}
+	
+	switch(num%2) {
+	case 0:
+		System.out.println(num + "는 짝수");
+		break;
+	default:
+		System.out.println(num + "는 홀수");
+	}
+	
+	scan.close();
+	
+	
+	}
+
+}
