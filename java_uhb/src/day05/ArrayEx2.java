4fbe8ef2d8c0668933d58b7509b16fac7f7faf2c
umheabin <luv9685@naver.com>
2023-12-14 17:41:14 +0900

    자바 5일차 Array 예제

---
+++ java_uhb/src/day05/ArrayEx2.java
@@ -0,0 +1,20 @@
+package day05;
+
+public class ArrayEx2 {
+	public static void main(String[] args) {
+		int []arr1;
+		//자료형 []배열
+		
+		int[] arr[];
+		//자료형 배열[]
+		
+		//arr3 : 배열 , arr4 : 배열
+		int []arr3,arr4;
+		
+		//arr5 : 배열 , arr6 : 변수
+		
+		
+		int arr5[],arr6;
+	}
+
+}
