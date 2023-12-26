ae05084c0cc77eaefbea59e6c9fff62139d280f6
umheabin <luv9685@naver.com>
2023-12-26 17:53:08 +0900

    자바 12일차 모니터크기

---
+++ java_uhb/src/day12/product/TV.java
@@ -0,0 +1,21 @@
+package day12.product;
+
+public class TV extends Product {
+    int size; //모니터크기
+
+	public TV(String brand, String code, String name, int size) {
+		super(brand, code, name);
+		this.size = size;
+	}
+	
+	@Override
+	public void print() {
+		System.out.println("------------");
+		System.out.println("브랜드 : " + brand);
+		System.out.println("제품명 : " + name);
+		System.out.println("제품코드 : " + code);
+		System.out.println("화면크기 : " + size + "cm");
+	}
+    
+    
+}
