8a8277e025ee68c43b0e4994ae5ad16038157938
umheabin <luv9685@naver.com>
2023-12-26 17:50:13 +0900

    자바 11일차 두 정수와 산술 연산자

---
+++ java_uhb/src/day11/HomeworkEx2.java
@@ -0,0 +1,34 @@
+package day11;
+
+public class HomeworkEx2 {
+	/*두 정수와 산술 연산자를 입력받아 결과를 출력하는 코드를 작성하시오.
+	 * 메서드를 생성
+	 */
+	public static void main(String[] args) {
+		int num1 = 1,num2 = 2;
+		char operator = '/';
+		double res = arithmeticCaculator(num1, operator, num2);
+		System.out.println(""+num1 + operator + num2 + "=" + res);
+	}
+	/**두 정수와 산술 연산자가 주어지면 산술 연산 결과를 알려주는 메서드
+	 * @parm num1 정수1
+	 * @parm num2 정수2
+	 * @parm operator 산술연산자
+	 * @return 정수1과 정수2를 산술연산한 결과
+	 * 
+	 */
+	public static int arithmeticCaculator(int num1, char operator, int num2) {
+		switch(operator) {
+		case '+': return num1 + num2;
+		case '-': return num1 - num2;
+		case '*': return num1 * num2;
+		case '%': return num1 % num2;
+		
+		}
+		//산술 연산자가 아닌 경우,아직은 예외처리를 안배워서 0으로 리턴
+		//그래서 0이 리턴되는 경우 1-1인지,1*0인지,4%2인지,0/2인지
+		//잘못된 연산자 때문인지 구분이 안됨.
+		return 0;
+	}
+
+}
