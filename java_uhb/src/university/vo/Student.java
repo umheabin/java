b6deb4a3fd6870ab704a5c7a5652c1b650acf7b6
umheabin <luv9685@naver.com>
2024-01-12 17:42:32 +0900

    대학생 과제

---
+++ java_uhb/src/university/vo/Student.java
@@ -0,0 +1,14 @@
+package university.vo;
+
+
+/**대학생 정보를 나타내는 클래스
+ * - 대학생 정보에 필요하다고 생각되는 정보를 멤버 변수로 추가해보세요. 
+ * */
+public class Student {
+     public String Student;  //학생
+     public String professor; //교수
+     public String Department; //학과
+     public String lecture; //강의
+     public int credit; //학점
+     
+}
