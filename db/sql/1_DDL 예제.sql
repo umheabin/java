6d4e18b0562e85ffccbc742cd93ef5c262c68996
umheabin <87254253+umheabin@users.noreply.github.com>
2024-02-12 23:47:32 +0900

    Create 1_DDL 예제.sql

---
+++ db/sql/1_DDL 예제.sql
@@ -0,0 +1,8 @@
+/* 여러줄 주석
+*/
+# 데이터베이스 추가(없으면)
+# create database if not exists test;
+# create schema if not exists test;
+# 데이터베이스 삭제(있으면)
+# drop database if exists test;
+# drop schema if exists test;
